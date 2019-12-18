3da389136723706e434d8fac3ba327f0fdfc1c91
thbdias <thiagobalbino91@gmail.com>
2019-12-18 07:52:04 -0300

    implementacao da autenticacao do usuario

--- WebContent/pages/autenticar.jsp
+++ WebContent/pages/autenticar.jsp
@@ -8,6 +8,23 @@
 </head>
 <body>
 	<h1>Autenticar Usuário</h1>
+	
+	<form action="serviletAutenticacao" method="post">
+		<table>
+			<tr>
+				<td>Login:</td>
+				<td><input type="text" id="login" name="login"></td>
+			</tr>
+			<tr>
+				<td>Senha:</td>
+				<td><input type="password" id="senha" name="senha"></td>
+			</tr>
+			<tr>
+				<td></td>
+				<td><input type="submit" id="logar" name="logar" value="Logar"></td>
+			</tr>
+		</table>
+	</form>
 
 </body>
 </html>
---
+++ src/servlet/ServiletAutenticacao.java
@@ -0,0 +1,54 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import user.UserLogado;
+
+@WebServlet("/pages/serviletAutenticacao")
+public class ServiletAutenticacao extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public ServiletAutenticacao() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String login = request.getParameter("login");
+		String senha = request.getParameter("senha");
+		
+		// neste momento pode ser feito uma validação no bando de dados
+		if (login.equalsIgnoreCase("admin") && senha.equalsIgnoreCase("123")) {
+			// se o login foi bem sucedido			
+
+			UserLogado user = new UserLogado();
+			user.setLogin(login);
+			user.setSenha(senha);
+			
+			//setar usuario na sessao
+			HttpServletRequest req = (HttpServletRequest) request;
+			HttpSession session = req.getSession();
+			session.setAttribute("usuario", user);
+			
+			//redireciona para o sistema autorizar
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/pages/acessoAoSistema.jsp");
+			dispatcher.forward(request, response);
+		}
+		else {
+			//se o login falou redireciona para login novamente
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/pages/autenticar.jsp");
+			dispatcher.forward(request, response);
+		}
+	}
+
+}
