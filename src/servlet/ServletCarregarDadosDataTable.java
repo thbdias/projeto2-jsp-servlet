c48d9eb308c921d85b3d9d1597405e595bdbcf0f
thbd <thiagobalbino91@gmail.com>
2019-12-18 22:13:38 -0300

    create servlet datatable

---
+++ src/servlet/ServletCarregarDadosDataTable.java
@@ -0,0 +1,24 @@
+package servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/pages/carregarDadosDataTableServlet")
+public class ServletCarregarDadosDataTable extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public ServletCarregarDadosDataTable() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	}
+
+}
