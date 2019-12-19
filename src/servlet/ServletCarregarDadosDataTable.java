6b46095b824a5cda004ab0142f4f22eedd624c73
thbd <thiagobalbino91@gmail.com>
2019-12-18 22:29:17 -0300

    datatable jquery server side - parte 2

--- src/servlet/ServletCarregarDadosDataTable.java
+++ src/servlet/ServletCarregarDadosDataTable.java
@@ -16,6 +16,33 @@ public class ServletCarregarDadosDataTable extends HttpServlet {
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String json = "{" + 
+					  "\"draw\": 1, " +
+					  "\"recordsTotal\": 57, " +
+					  "\"recordsFiltered\": 57, " +
+					  "\"data\": [ " +
+					    "[ " +
+					      "\"Airi\", " +
+					      "\"Satou\", " +
+					      "\"Accountant\", " +
+					      "\"Tokyo\", " +
+					      "\"28th Nov 08\", " +
+					      "\"$162.700\" "  +
+					    "], " +
+					    "[ " +
+					      "\"Angelica\", " +
+					      "\"Ramos\", " +
+					      "\"Chief Executive Officer (CEO)\", " +
+					      "\"London\", " +
+					      "\"9th Oct 09\", " +
+					      "\"$1.200.000\" " +
+					    "] " +			    
+					  "] " +
+					"} ";
+	
+		response.setStatus(200); //resposta completa ok
+		response.getWriter().write(json); //json de resposta (escreve a resposta Http)
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
